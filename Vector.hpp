65dc010033da0d4978b99c61764eac2de485c0c3
hayk003l <haykharutyunyan113@gmail.com>
2024-05-30 15:16:50 +0400

    adding functions

--- Vector.hpp
+++ Vector.hpp
@@ -45,6 +45,81 @@ namespace myStl {
             _ptr[_size++] = elem;
         }
 
+        void pop_back() {
+            if (_size == 0) {
+                std::cout << "Error. Wrong operation." << std::endl;
+            }
+            else {
+                _ptr[--_size] = 0;
+            }
+        }
+
+        void push_front(const T& elem) {
+            if (_size >= _cap) {
+                _cap *= 2;
+                T* temp = new T[_cap];
+                for (int i = 0; i < _size; ++i) {
+                    temp[i] = _ptr[i];
+                }
+                for (int i = _size; i < _cap; ++i) {
+                    temp[i] = 0;
+                }
+                delete[] _ptr;
+                _ptr = temp;
+                temp = nullptr;
+            }
+            for (int i = 0; i < _size; ++i) {
+                _ptr[i + 1] = _ptr[i];
+            }
+            _ptr[0] = elem;
+            ++_size;
+        }
+
+        void pop_front() {
+            for (int i = 0; i < _size - 1; ++i) {
+                _ptr[i] = _ptr[i + 1];
+            }
+            --_size;
+        }
+
+        void insert(const int& index, const T& val) {
+            if (index < 0 || index > _cap) {
+                std::cout << "Error. Wrong operation." << std::endl;
+            }
+            else {
+                if (index == _cap || _size == _cap) {
+                    _cap *= 2;
+                    T* temp = new T[_cap];
+                    for (int i = 0; i < _size; ++i) {
+                        temp[i] = _ptr[i];
+                    }
+                    for (int i = _size; i < _cap; ++i) {
+                        temp[i] = 0;
+                    }
+                    delete[] _ptr;
+                    _ptr = temp;
+                    temp = nullptr;
+                }
+                if (index > _size) {
+                    _ptr[index] = val;
+                    _size = index;
+                }
+                else {
+                    for (int i = index; i < _size; ++i) {
+                        _ptr[i + 1] = _ptr[i];
+                    }
+                    _ptr[index] = val;
+                    ++_size;
+                }
+            }
+        }
+
+        int getSize() const {
+            return _size;
+        }
+        int getCap() const {
+            return _cap;
+        }
     private:
         int _size = 0;
         int _cap = 10;
--- main.cpp
+++ main.cpp
@@ -13,8 +13,23 @@ int main() {
     vec.push_back(5);
     vec.push_back(5);
     vec.push_back(5);
-    vec.push_back(5);
-    vec.push_back(5);
-    std::cout << vec[0] << std::endl;
-    std::cout << vec[15] << std::endl;
+    vec.push_back(5);    
+    vec.push_back(5);    
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    vec.push_front(1);
+    std::cout << vec.getCap() << std::endl;
+    return 0;
 }
